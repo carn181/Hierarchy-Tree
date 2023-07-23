5562c641995d10bdd51266ff3a0b728a9a30e15c
sambdal <yarnrav@gmail.com>
2023-07-22 22:16:37 -0400

    Pointers instead of copying objects

--- src/hierarch.cpp
+++ src/hierarch.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
+#include "util.h"
 #include <bits/stdc++.h>
-#include <string>
 #include <cairomm/context.h>
 #include <cairomm/fontface.h>
 #include <cairomm/surface.h>
-#include <cairommconfig.h>
 #include <cairomm/types.h>
-#include "util.h"
+#include <cairommconfig.h>
+#include <cstddef>
+#include <iostream>
+#include <string>
 
 struct Tree {
   std::string text;
@@ -14,25 +15,29 @@ struct Tree {
   double x, y;
   double mod = 0.0;
   double width, height;
+  double top_height;
 
   Tree(std::string txt, std::vector<Tree> chldrn = {}) {
     text = txt;
     children = chldrn;
   }
 
-  void calc_text(Cairo::RefPtr<Cairo::Context> cr, int size) {
+  void calc_text(Cairo::RefPtr<Cairo::Context> cr, double size) {
     height = 0; // JUST TO BE SURE
     Cairo::TextExtents extents;
     auto str_list = splitstring(text, '\n');
     cr->set_font_size(size);
 
+    cr->get_text_extents(str_list.front(), extents);
+    top_height = extents.height;
+
     for (const auto &str : str_list) {
       cr->get_text_extents(str, extents);
 
       width = std::max(width, extents.width);
-
       height += extents.height;
     }
+    height += 5;
   }
 };
 
@@ -41,6 +46,7 @@ private:
   Cairo::RefPtr<Cairo::ToyFontFace> font;
   std::string filename;
   double size;
+  double xsep, ysep;
 
 public:
   Cairo::RefPtr<Cairo::Context> cr;
@@ -48,6 +54,8 @@ public:
     double width = 1920;
     double height = 1080;
     size = font_size;
+    xsep = size / 1.0;
+    ysep = size / 1.0;
     filename = file;
 
     auto surface = Cairo::SvgSurface::create(filename, width, height);
@@ -75,39 +83,38 @@ public:
     cr->save();
   }
 
-  void draw_text(int x, int y, int size, std::string s) {
+  void draw_text(double x, double y, std::string s) {
     auto str_list = splitstring(s, '\n');
     cr->set_font_size(size);
-    for (auto i = 0ul; i < str_list.size(); i++) {
-      cr->move_to(x, y + i * size);
+    for (size_t i = 0; i < str_list.size(); i++) {
+      cr->move_to(x, y + static_cast<double>(i) * size);
       cr->show_text(str_list[i]);
     }
   }
-  
 
   // Check this func for errors
-  void draw_paren(int x, int y, int y1, int y2, double width) {
+  void draw_paren(double x, double y, double y1, double y2, double width) {
     cr->set_source_rgba(0.0, 0.0, 0.0, 0.7);
     // Parenthesis
     auto x1 = x + 2 * width / 3;
     //    std::cout << y1 << " " << y << " " << y2 << '\n';
 
     // Upper Curve
-    auto height = y-y1;
+    auto height = y - y1;
     cr->move_to(x, y);
     cr->line_to(x + width / 3, y);
-    cr->curve_to(x1, y, x1, y-height*0.3, x1, y-height*0.3);
-    cr->line_to(x1, y1+height*0.3);
+    cr->curve_to(x1, y, x1, y - height * 0.3, x1, y - height * 0.3);
+    cr->line_to(x1, y1 + height * 0.3);
     cr->curve_to(x1, y1, x1 + width / 3, y1, x1 + width / 3, y1);
     cr->line_to(x + width, y1);
 
     // Lower Curve
-    height = y2-y;
+    height = y2 - y;
     cr->move_to(x, y);
     cr->line_to(x + width / 3, y);
 
-    cr->curve_to(x1, y, x1, y+height*0.3, x1, y+height*0.3);
-    cr->line_to(x1, y2-height*0.3);
+    cr->curve_to(x1, y, x1, y + height * 0.3, x1, y + height * 0.3);
+    cr->line_to(x1, y2 - height * 0.3);
     cr->curve_to(x1, y2, x1 + width / 3, y2, x1 + width / 3, y2);
     cr->line_to(x + width, y2);
 
@@ -127,14 +134,6 @@ public:
     }
   }
 
-  double inline ysep(double size){
-    return size / 3.0;
-  }
-
-  double inline xsep(double size){
-    return size / 1.0;
-  }
-  
   int first_pass(Tree *t, double x, double y, int start = -1) {
     if (start == -1)
       t->calc_text(cr, size);
@@ -143,24 +142,28 @@ public:
     //    draw_text(t->x, t->y, size, t->text);
 
     if (t->children.size() > 0) {
-      auto k = 0;
+      double k = 0;
       for (auto &child : t->children) {
         child.calc_text(cr, size);
-        first_pass(&child, t->x+t->width+xsep(size), y + k, 0);
-        k += child.height+ysep(size);
+        first_pass(&child, t->x + t->width + xsep, y + k, 0);
+        k += child.height + ysep;
       }
     }
-    if(!t->children.empty()){
-      if(t->children.size()!=1)
-	t->y = ((t->children.back().y + t->children.front().y - t->children.front().height) / 2.0);
+    if (!t->children.empty()) {
+      if (t->children.size() != 1) {
+        std::printf("Centering %s\n", t->text.c_str());
+        t->y = ((t->children.back().y +
+                 (t->children.front().y - t->children.front().height)) /
+                2.0);
+      }
     }
     return 0;
   }
 
-#define LOG(format, ...)			\
-  {						\
-    print_tabs(depth);				\
-    std::printf(format, __VA_ARGS__);		\
+#define LOG(format, ...)                                                       \
+  {                                                                            \
+    print_tabs(depth);                                                         \
+    std::printf(format, __VA_ARGS__);                                          \
   }
 
   // TODO: Need to equal almost equal floats (499.9999 and 500.0)
@@ -181,43 +184,55 @@ public:
     for (size_t i = 0, j = 1;
          i <= t->children.size() - 1 && j <= t->children.size() - 1; i++, j++) {
       double xl, xr, yl, yr;
-      Tree l_contour = t->children.at(i);
-      Tree r_contour = t->children.at(j);
+      Tree *l_contour = &t->children.at(i);
+      Tree *r_contour = &t->children.at(j);
 
       do {
-        LOG("%s<%s,%s>: LOOP START MOD: %f\n", t->text.c_str(), r_contour.text.c_str()
-	    , l_contour.text.c_str(), l_contour.mod);
-        yl = l_contour.y;
-        yr = r_contour.y-r_contour.height;
+        LOG("%s<%s,%s>: LOOP START MOD: %f\n", t->text.c_str(),
+            r_contour->text.c_str(), l_contour->text.c_str(), l_contour->mod);
+        yl = l_contour->y;
+        yr = r_contour->y - r_contour->height;
 
-        if (yl+l_contour.mod > yr) {
+        if (yl + l_contour->mod > yr + r_contour->mod) {
           // move by yr-l
           print_char('=', 100);
-	  std::cout << '\n';
-          LOG("MOVE %s BY %f\n", t->children.at(j).text.c_str(), yl - yr);
-	  t->children.at(j).mod = yl+l_contour.mod - yr + ysep(size);
+          std::cout << '\n';
+          LOG("MOVE %s BY %f\n", t->children.at(j).text.c_str(),
+              yl - yr + l_contour->mod + ysep);
+          t->children.at(j).mod = std::max(
+              t->children.at(j).mod, (yl + l_contour->mod) - (yr) + ysep + 10);
           print_char('=', 100);
-	  std::cout << '\n';	  
+          std::cout << '\n';
         }
-        xr = r_contour.x+r_contour.width;
-        xl = l_contour.x+l_contour.width;
+        xr = r_contour->x + r_contour->width;
+        xl = l_contour->x + l_contour->width;
 
-        LOG("%s<%s,%s>: %f,%f vs %f,%f\n", t->text.c_str(),
-            r_contour.text.c_str(), l_contour.text.c_str(), xr, yr, xl, yl);
+        LOG("%s<%s,%s>: %f, %f vs %f, %f\n", t->text.c_str(),
+            r_contour->text.c_str(), l_contour->text.c_str(), xr, yr, xl, yl);
 
         if (xl <= xr) {
-          if (l_contour.children.empty())
+          if (l_contour->children.empty())
             break;
-          l_contour = l_contour.children.back();
+          LOG("CHANGING L_CONTOUR FROM %s, size = %lu to %s, size = %lu\n",
+              l_contour->text.c_str(), l_contour->children.size(),
+              l_contour->children.front().text.c_str(),
+              l_contour->children.front().children.size());
+          l_contour = &l_contour->children.back();
+          LOG("CHANGED L_CONTOUR TO %s\n", l_contour->text.c_str());
         }
         if (xl >= xr) {
-          if (r_contour.children.empty())
+          if (r_contour->children.empty())
             break;
-          r_contour = r_contour.children.front();
+          LOG("CHANGING R_CONTOUR FROM %s, size = %lu to %s, size = %lu\n",
+              r_contour->text.c_str(), r_contour->children.size(),
+              r_contour->children.front().text.c_str(),
+              r_contour->children.front().children.size());
+          r_contour = &r_contour->children.front();
+          LOG("CHANGED R_CONTOUR TO %s\n", r_contour->text.c_str());
         }
 
-        LOG("%s<%s,%s>: LOOP DONE\n", t->text.c_str(), r_contour.text.c_str(),
-            l_contour.text.c_str());
+        LOG("%s<%s,%s>: LOOP DONE\n", t->text.c_str(), r_contour->text.c_str(),
+            l_contour->text.c_str());
 
       } while (1);
     }
@@ -226,19 +241,27 @@ public:
   }
 
   int draw_subtree(Tree t, double mod = 0.0) {
-    draw_text(t.x, t.y+mod+t.mod, size, t.text);
+    draw_text(t.x, t.y + mod + t.mod, t.text);
     for (auto &child : t.children) {
-      draw_subtree(child, mod+t.mod);
+      draw_subtree(child, mod + t.mod);
     }
     std::cout << "Drawing " << t.text << "= {";
-    for(auto &child: t.children){
-      std::cout << child.text << "(" << child.y+mod+child.mod << "),";
+    for (auto &child : t.children) {
+      std::cout << child.text << "(" << child.y + mod + child.mod << "),";
     }
     std::cout << "}\n";
-    
-    if(!t.children.empty()){
-      draw_paren(t.x+t.width, t.y-t.height/2+mod+t.mod, t.children.front().y+mod+t.mod-t.children.front().height,
-  	t.children.back().y+mod+t.mod,xsep(size));
+
+    if (!t.children.empty()) {
+      double x = t.x + t.width;
+      double y = t.y - (t.height / 2.0) + mod + t.mod;
+      double y1 = t.children.front().y + mod + t.mod + t.children.front().mod -
+                  t.children.front().top_height;
+      double y2 = t.children.back().y + mod + t.mod + t.children.back().mod;
+      double width = xsep;
+      draw_paren(x, y, y1, y2, width);
+      //      draw_paren(t.x+t.width, t.y-t.height/2+mod+t.mod,
+      //      t.children.front().y+mod+t.mod-t.children.front().height,
+      //  	t.children.back().y+mod+t.mod,xsep(size));
     }
     return 0;
   }
--- src/test.cpp
+++ src/test.cpp
@@ -12,29 +12,53 @@ int test_newmodel() {
   HierarchyTree t("new-tree.svg",20);
   t.set_background();
   auto tr = Tree{
-      "Root",
-      {{"First",
-        {{"Second", {{"Third", {{"Fo"}, {"Fou", {{"Fifth"}}}}}, {"Third"}}},
-         {"Second",
-          {{"Third", {{"Fourth"}, {"Fourth"}}},
-           {"Third", {{"Fourth"}, {"Fourth"}}}}}}},
-       {"First"}}};
+    "Root",
+    {{"First",
+      {{"Second", {{"Third", {{"Fo"}, {"Fou", {{"Fifth"}}}}}, {"Third"}}},
+       {"Second",
+	{{"Third", {{"Fourth"}, {"Fourth"}}},
+	 {"Third", {{"Fourth"}, {"Fourth"}}}}}}},
+     {"First"}}};
   auto simple_tr = Tree{
     "1",
     {{{"2", {{"5"}, {"6"}}},
       {"3", {{"4"}}}}}
   };
-    auto const goal = Tree{
-      "In P.Rami disciplina Philosophica, consider.",
-      {{"Idea illius disciplinus, expressa \nin Ciceroniano, qui exemplo "
-        "instituti Ciceronis"},
-       {"Curriculum opus Philosophicum,partum",
-        {{"Exotericum in",
-          {{"Grammatica", {{"Latina"}, {"Graeca"}, {"Gallica"}}},
-           {"Logicaseu Dialectica"}}},
-         {"Acroamaticu in", {{"Arithmetica"}, {"Geometria"}, {"Physica"}}}}}}};
+  auto const goal = Tree{
+    "In P.Rami disciplina Philosophica, consider.",
+    {{"Idea illius disciplinus, expressa \nin Ciceroniano, qui exemplo "
+       "instituti Ciceronis"},
+     {"Curriculum opus Philosophicum,partum",
+      {{"Exotericum in",
+	{{"Grammatica", {{"Latina"}, {"Graeca"}, {"Gallica"}}},
+	 {"Logicaseu Dialectica"}}},
+       {"Acroamaticu in", {{"Arithmetica"}, {"Geometria"}, {"Physica"}}}}}}};
 
-  t.draw_diagram(tr, 20, 500);
+  auto const goal_2 =
+    Tree{"Justifying Faith",
+	 {{"Definition",
+	   {{"\"Certaine Knowledge\""},
+	    {"\"Sure Persuasion\""},
+	    {"\"Of the free favour of God\""},
+	    {"\"In Jesus Christ\""},
+	    {"\"Sealed in our heartes by the holy Ghost\""}}},
+	  {"Fruits of Faith",
+	   {{"Inward Godliness",
+	     {{"Remission of sins"},
+	      {"Love of the Father"},
+	      {"Promise that prayers will be heard"}}},
+	    {"Outward Godliness",
+	     {{"Obedience",
+	       {{"Obey", {{"Reasons"}, {"Laws"}}},
+		{"Fulfill", {{"Doctrine"}, {"Uses"}}}
+	       }
+	       },
+	      {"Ends",
+	       {{"For God's Glory"},
+		{"For our own comfort"},
+		{"For the good of our brethren"}}}}}}}}};
+
+  t.draw_diagram(goal_2, 20, 300);
   t.export_svg();
   return 0;
 }
@@ -43,9 +67,9 @@ int test_drawparen() {
   HierarchyTree t("paren.svg",20);
   t.set_background();
 
-  t.draw_paren(400, 100+500/2, 100, 500, 20);
-  t.draw_text(420, 100 + 20, 20, "Test");
-  t.draw_text(420, 500, 20, "Test");
+  t.draw_paren(400, 100+500.0/2, 100, 500, 20);
+  t.draw_text(420, 100 + 20, "Test");
+  t.draw_text(420, 500, "Test");
   t.export_svg();
 
   return 0;
